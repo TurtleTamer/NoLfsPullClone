3ebf3e2c9c3af0b9427100bbd41d6693a381b18b
Lee Chun Kit <4178937+TurtleTamer@users.noreply.github.com>
2017-11-07 16:37:38 +0800

    initial commit 1

---
+++ CreateLfsObjects.bat
@@ -0,0 +1,5 @@
+md .git\lfs\objects\c1\31
+copy b.jpg .git\lfs\objects\c1\31\c131925f5a09e3fb1a6c2a3c1065ee4ff7585cdf25d12944adc7c2ae5a6f5d62
+md .git\lfs\objects\47\f0
+copy a.jpg .git\lfs\objects\47\f0\47f068122b357ff753e7b8a42fa0b89ed981074ddba07befcf8e9f3318b755ab
+pause
